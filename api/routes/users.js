e9fbf1173052937554d7aa2b055cd5ce18b4420f
andersoncasantos <andersonmtv07@gmail.com>
2023-07-28 14:55:17 -0300

    calling routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,14 @@
 import express from "express";
-import {getUsers} from "../controllers/user.js";
+import {getUsers, addUser, updateUser, deleteUser} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+
+router.post("/", addUser);
+
+router.post("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
 
 export default router; 
