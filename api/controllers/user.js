260361dfb378889da3b9f082e6a52b9f74d81295
andersoncasantos <andersonmtv07@gmail.com>
2023-07-28 14:43:19 -0300

    create function addUser

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -9,3 +9,20 @@ export const getUsers = (_, res) => {
         return res.status(200).json(data);
     });
 };
+
+export const addUser = (req, res) => {
+    const search =
+        "INSERT INTO compromissos(`nome`, `descricao`, `data_hora`) VALUES(?)";
+    
+    const values = [
+        req.body.nome,
+        req.body.descricao,
+        req.body.data_hora,
+    ];
+
+    db.query(search, [values], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Compromisso criado com sucesso!");
+    }); 
+};
